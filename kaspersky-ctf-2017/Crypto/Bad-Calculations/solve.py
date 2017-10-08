101fc38ed88a66997c7cf49970f8cf29459aa590
Jonathan Jacobi <hubh88@gmail.com>
2017-10-09 01:01:27 +0300

    Update solve.py

--- kaspersky-ctf-2017/Crypto/Bad-Calculations/solve.py
+++ kaspersky-ctf-2017/Crypto/Bad-Calculations/solve.py
@@ -2,10 +2,6 @@ from base64 import b64decode
 
 b = 22 # the "magic constant" given in the original script
 
-# After finding out that the flag is the following result: (The hard part of the challenge is simplifying the code, and figurint what the encryption does)
-# for each letter in the plaintext, the value of the corresponding letter in the ciphertext is:
-# ciphertext = plaintext_letter + 22
-
 cipher_base64 = "hnd/goJ/e4h1foWDhYOFiIZ+f3l1e4R5iI+Gin+FhA=="
 decoded_base64 = b64decode(cipher_base64)
 plaintext = ""
