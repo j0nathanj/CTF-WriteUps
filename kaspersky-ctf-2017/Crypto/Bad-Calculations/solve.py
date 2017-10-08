e9db9bdfa2bec86a2d9a8918f8dfcd56de3d2c39
Jonathan Jacobi <hubh88@gmail.com>
2017-10-09 00:53:01 +0300

    Update solve.py

--- kaspersky-ctf-2017/Crypto/Bad-Calculations/solve.py
+++ kaspersky-ctf-2017/Crypto/Bad-Calculations/solve.py
@@ -1,16 +1,11 @@
-from random import choice
-from sys import argv
 from base64 import b64encode
 from base64 import b64decode
-from math import sqrt
 
 b = 22 # the "magic value" given in the script
 
 # After finding out that the flag is the following result: (The hard part of the challenge is simplifying the code, and figurint what the encryption does)
 # for each letter in the plaintext, the value of the corresponding letter in the ciphertext is:
 # ciphertext = plaintext_letter + 22
-# what we need to do is the following
-
 
 cipher_base64 = "hnd/goJ/e4h1foWDhYOFiIZ+f3l1e4R5iI+Gin+FhA=="
 decoded_base64 = b64decode(cipher_base64)
@@ -20,6 +15,6 @@ for character in decoded_base64:
 	source_character = ord(character) - b
 	plaintext += chr(source_character)
 
-print plaintext # This is the flag
+print plaintext # At this point, the plaintext holds the flag.
 
 print "Flag: KLCTF{"+str(plaintext)+"}"
